8535e279c20f8ea9398fe8847410cc860ea7b377
Eldar Amar <eldar.amar@mail.huji.ac.il>
2025-04-14 12:58:25 +0300

    ChatFix aliasing (maybe)

--- q5.sql
+++ q5.sql
@@ -1,13 +1,13 @@
-SELECT name FROM
-
-(SELECT uid FROM membersInKnesset) All
+SELECT name
+FROM
+  (SELECT uid FROM membersInKnesset) AS AllUIDs
 EXCEPT
-(SELECT uid FROM
-    (SELECT number AS num1, uid FROM membersInKnesset) K1
-    NATURAL JOIN
-    (SELECT number AS num2, uid FROM membersInKnesset) K2
-WHERE num1 != num2;)
+  (SELECT uid
+   FROM
+     (SELECT number AS num1, uid FROM membersInKnesset) AS K1
+     NATURAL JOIN
+     (SELECT number AS num2, uid FROM membersInKnesset) AS K2
+   WHERE num1 != num2) AS InTwoKnessets
 
 NATURAL JOIN
-
-(SELECT * FROM members WHERE birthPlace = "Jerusalem")
+  (SELECT * FROM members WHERE birthPlace = 'Jerusalem') AS JMems;
