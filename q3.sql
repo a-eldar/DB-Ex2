78af092c3cd956981c130e2e36a4382db01d967f
Eldar Amar <eldar.amar@mail.huji.ac.il>
2025-04-14 11:55:32 +0300

    fixed knessets

--- q3.sql
+++ q3.sql
@@ -1,4 +1,4 @@
 SELECT DISTINCT name, number
-FROM members NATURAL JOIN memberInKnesset NATURAL JOIN knesset
+FROM members NATURAL JOIN memberInKnesset NATURAL JOIN knessets
 WHERE (party = 'Likud' OR party = 'Meretz') AND startYear - birthYear > 70
 ORDER BY name,number;
