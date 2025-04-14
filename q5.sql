204f4b4121781e67b608ec93f3ae748ed0b0517c
Eldar Amar <eldar.amar@mail.huji.ac.il>
2025-04-14 12:54:49 +0300

    added aliases in FROM

--- q5.sql
+++ q5.sql
@@ -1,11 +1,11 @@
 SELECT name FROM
 
-(SELECT uid FROM membersInKnesset)
+(SELECT uid FROM membersInKnesset) All
 EXCEPT
 (SELECT uid FROM
-    (SELECT number AS num1, uid FROM membersInKnesset)
+    (SELECT number AS num1, uid FROM membersInKnesset) K1
     NATURAL JOIN
-    (SELECT number AS num2, uid FROM membersInKnesset)
+    (SELECT number AS num2, uid FROM membersInKnesset) K2
 WHERE num1 != num2;)
 
 NATURAL JOIN
