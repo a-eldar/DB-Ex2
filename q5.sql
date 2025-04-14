a9189e5e87e6b9e4d86aa4ca852daa8987ed7f46
Eldar Amar <eldar.amar@mail.huji.ac.il>
2025-04-14 13:04:58 +0300

    please work

--- q5.sql
+++ q5.sql
@@ -1,12 +1,12 @@
 SELECT name
 FROM (
-    (SELECT uid FROM membersInKnesset)
+    (SELECT uid FROM memberInKnesset)
     EXCEPT
     (SELECT uid
      FROM
-       (SELECT number AS num1, uid FROM membersInKnesset) AS K1
+       (SELECT number AS num1, uid FROM memberInKnesset) AS K1
        NATURAL JOIN
-       (SELECT number AS num2, uid FROM membersInKnesset) AS K2
+       (SELECT number AS num2, uid FROM memberInKnesset) AS K2
      WHERE num1 != num2)
 ) AS AllInOneKnesset
 NATURAL JOIN
