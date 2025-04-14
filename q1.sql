782542f973e0456884636e34c531f6fd62a89bba
Eldar Amar <eldar.amar@mail.huji.ac.il>
2025-04-14 11:40:45 +0300

    fixed Of -> of

--- q1.sql
+++ q1.sql
@@ -1,3 +1,3 @@
 SELECT DISTINCT name
 FROM members
-WHERE birthYear > 1970 AND educatedAt = 'Hebrew University Of Jerusalem';
+WHERE birthYear > 1970 AND educatedAt = 'Hebrew University of Jerusalem';
