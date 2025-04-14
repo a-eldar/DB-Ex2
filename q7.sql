cccca168a4c9d2ef02ff6b7b8151b5fb33bcea67
Eldar Amar <eldar.amar@mail.huji.ac.il>
2025-04-14 13:20:26 +0300

    fixed EXCEPT alias

--- q7.sql
+++ q7.sql
@@ -1,14 +1,15 @@
-
 SELECT number, name
 FROM 
 (
     SELECT uid, number
     FROM memberInKnesset
     EXCEPT
-    (SELECT m2.uid, m2.number
-    FROM (members NATURAL JOIN memberInKnesset) AS m1
-    INNER JOIN (members NATURAL JOIN memberInKnesset) AS m2
-    WHERE m1.number = m2.number AND m1.birthYear < m2.birthYear) AS NA
+    (
+        SELECT m2.uid, m2.number
+        FROM (members NATURAL JOIN memberInKnesset) AS m1
+        INNER JOIN (members NATURAL JOIN memberInKnesset) AS m2
+        WHERE m1.number = m2.number AND m1.birthYear < m2.birthYear
+    )
 ) AS A
 NATURAL JOIN members
 ORDER BY number, name;
