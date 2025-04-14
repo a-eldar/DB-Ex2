a977e6cff573fa0091d77c5339f9126e928116b5
Eldar Amar <eldar.amar@mail.huji.ac.il>
2025-04-14 13:03:43 +0300

    chat2

--- q5.sql
+++ q5.sql
@@ -1,13 +1,13 @@
 SELECT name
-FROM
-  ((SELECT uid FROM membersInKnesset) AS AllUIDs
-EXCEPT
-  (SELECT uid
-   FROM
-     (SELECT number AS num1, uid FROM membersInKnesset) AS K1
-     NATURAL JOIN
-     (SELECT number AS num2, uid FROM membersInKnesset) AS K2
-   WHERE num1 != num2))
-
+FROM (
+    (SELECT uid FROM membersInKnesset)
+    EXCEPT
+    (SELECT uid
+     FROM
+       (SELECT number AS num1, uid FROM membersInKnesset) AS K1
+       NATURAL JOIN
+       (SELECT number AS num2, uid FROM membersInKnesset) AS K2
+     WHERE num1 != num2)
+) AS AllInOneKnesset
 NATURAL JOIN
-  (SELECT * FROM members WHERE birthPlace = 'Jerusalem');
+  (SELECT * FROM members WHERE birthPlace = 'Jerusalem') AS JerusalemMembers;
